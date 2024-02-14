49e079150bb442d5b2bb6793648daaedf19a51c1
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-14 12:36:57 +0100

    Authentication controller & routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,10 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+    ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+    ->name('logout');
