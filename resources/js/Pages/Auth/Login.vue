67b2c5f52894b70cc4e89f7b7efcdffd4ad76d66
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-14 12:47:00 +0100

    Login form markup & basic logic

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="login">
+        <div class="w-1/2 mx-auto">
+            <div>
+                <label class="label"> E-mail (username) </label>
+                <input
+                    id="email"
+                    type="text"
+                    class="input"
+                    v-model="form.email"
+                />
+                <div class="input-error">Potential errors</div>
+            </div>
+            <div class="mt-4">
+                <label class="label">Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    class="input"
+                    v-model="form.password"
+                />
+            </div>
+            <div class="mt-4">
+                <button class="btn-primary w-full" type="submit">Login</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script setup>
+const form = useForm({
+    email: null,
+    password: null,
+});
+
+const login = () => form.post(route("login.store"));
+</script>
