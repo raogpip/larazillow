f4639cf5c0bbe06de4f406afcba3976ea69e92f1
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-15 14:49:20 +0100

    Register link in nav bar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -28,7 +28,8 @@
                         >
                     </div>
                 </div>
-                <div v-else>
+                <div v-else class="flex items-center gap-4">
+                    <Link :href="route('user-account.create')">Register</Link>
                     <Link :href="route('login')">Sign-In</Link>
                 </div>
             </nav>
