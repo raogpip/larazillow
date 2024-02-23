827479c77bbc0ff627adca2d3fc80f4f2df224d8
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-23 19:39:34 +0100

    Filtering by deleted listings back-end

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -14,11 +14,21 @@ class RealtorListingController extends Controller
         $this->authorizeResource(Listing::class, 'listing');
     }
 
-    public function index()
+    public function index(Request $request)
     {
+        $filters = [
+            'deleted' => $request->boolean('deleted'),
+            ...$request->only(['by', 'order']),
+        ];
         return inertia(
             'Realtor/Index',
-            ['listings' => Auth::user()->listings]
+            [
+                'listings' => Auth::user()
+                    ->listings()
+                    //->mostRecent()
+                    ->filter($filters)
+                    ->get()
+            ]
         );
     }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -15,6 +15,10 @@ class Listing extends Model
     protected $fillable = [
         'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price'
     ];
+    protected $sortable = [
+        'price', 'created_at'
+    ];
+
 
     //setting up relations with user model
     public function owner(): BelongsTo
@@ -56,6 +60,14 @@ class Listing extends Model
             ->when(
                 $filters['areaTo'] ?? false,
                 fn ($query, $value) => $query->where('area', '<=', $value)
+            )->when(
+                $filters['deleted'] ?? false,
+                fn ($query, $value) => $query->withTrashed()
+            )->when(
+                $filters['by'] ?? false,
+                fn ($query, $value) =>
+                !in_array($value, $this->sortable) ? $query :
+                    $query->orderBy($value, $filters['order'] ?? 'desc')
             );
     }
 }
