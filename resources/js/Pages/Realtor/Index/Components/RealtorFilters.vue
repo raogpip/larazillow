e9ad9753c97cba59c81f0f3f2e3d2d846898ba1a
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-23 19:38:54 +0100

    Informing that the checkbox is active using vue watchers & preventing request spam using debouncing

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -3,6 +3,7 @@
         <div class="mb-4 mt-4 flex flex-wrap gap-2">
             <div class="flex flex-nowrap items-center gap-2">
                 <input
+                    v-model="filterForm.deleted"
                     id="deleted"
                     type="checkbox"
                     class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
@@ -13,4 +14,31 @@
     </form>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive, watch } from "vue";
+//import { Inertia } from "@inertiajs/inertia";
+import { router } from "@inertiajs/vue3";
+import { debounce } from "lodash";
+
+const filterForm = reactive({
+    deleted: false,
+});
+
+// watch(filterForm, () =>
+//     Inertia.get(route("realtor.listing.index"), filterForm, {
+//         preserveState: true,
+//         preserveScroll: true,
+//     })
+// );
+watch(
+    filterForm,
+    debounce(
+        () =>
+            router.get(route("realtor.listing.index"), filterForm, {
+                preserveState: true,
+                preserveScroll: true,
+            }),
+        1000
+    )
+);
+</script>
