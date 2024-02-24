c6c466b11e086b581e7987d5b7d061b809c0d98c
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-02-24 10:41:58 +0100

    Added images data model & relations between listings and images

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
@@ -27,6 +28,11 @@ class Listing extends Model
             \App\Models\User::class,
             'by_user_id'
         );
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
     }
 
 
