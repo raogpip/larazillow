0cb582f233501ec579431b624829785cc44c04ea
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-03-13 12:42:24 +0100

    No offer making & changing of listings that are sold

--- app/Http/Controllers/ListingOfferController.php
+++ app/Http/Controllers/ListingOfferController.php
@@ -11,6 +11,7 @@ class ListingOfferController extends Controller
 {
     public function store(Listing $listing, Request $request)
     {
+        $this->authorize('view', $listing);
         $listing->offers()->save(
             Offer::make(
                 $request->validate([
--- app/Policies/ListingPolicy.php
+++ app/Policies/ListingPolicy.php
@@ -26,7 +26,11 @@ class ListingPolicy
      */
     public function view(?User $user, Listing $listing): bool
     {
-        return true;
+        if ($user != null && $listing->by_user_id === $user->id) {
+            return true;
+        }
+
+        return $listing->sold_at === null;
     }
 
     /**
@@ -41,7 +45,8 @@ class ListingPolicy
      */
     public function update(User $user, Listing $listing): bool
     {
-        return $user->id === $listing->by_user_id;
+        return $listing->sold_at === null
+            && ($user->id === $listing->by_user_id);
     }
 
     /**
