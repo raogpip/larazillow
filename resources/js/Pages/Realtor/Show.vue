114f9fb9efd8d7be8f411a6eb206b290e3f69c31
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-03-16 10:39:17 +0100

    Added sold sign for the listing that are already sold

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -15,6 +15,7 @@
                 :key="offer.id"
                 :offer="offer"
                 :listing-price="listing.price"
+                :is-sold="listing.sold_at != null"
             />
         </div>
 
