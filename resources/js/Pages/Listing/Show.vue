c108275a1c42e39422fe4f4c52b217fd59f7c46a
raogpip <vitalik.fedorenko.2018@gmail.com>
2024-03-20 12:27:59 +0100

    Showing an empty state

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,21 +1,25 @@
 <template>
     <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-        <Box class="md:col-span-7 flex items-center w-full">
-            <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+        <Box
+            v-if="listing.images.length"
+            class="md:col-span-7 flex items-center w-full"
+        >
+            <div class="grid grid-cols-2 gap-1">
                 <img
                     v-for="image in listing.images"
                     :key="image.id"
                     :src="image.src"
                 />
             </div>
-            <div v-else class="w-full text-center font-medium text-gray-500">
-                No images
-            </div>
         </Box>
+        <EmptyState class="md:col-span-7 flex items-center w-full" v-else
+            >No images</EmptyState
+        >
         <div class="md:col-span-5 flex flex-col gap-4">
             <Box>
                 <template #header> Basic info </template>
                 <Price :price="listing.price" class="text-2xl font-bold" />
+
                 <ListingSpace :listing="listing" class="text-lg" />
                 <ListingAddress :listing="listing" class="text-gray-400" />
             </Box>
@@ -97,6 +101,7 @@ import ListingSpace from "@/Components/ListingSpace.vue";
 import Box from "@/Components/UI/Box.vue";
 import MakeOffer from "@/Pages/Listing/Show/Components/MakeOffer.vue";
 import OfferMade from "@/Pages/Listing/Show/Components/OfferMade.vue";
+import EmptyState from "@/Components/UI/EmptyState.vue";
 
 import { usePage } from "@inertiajs/vue3";
 
