548998feb0738dd18202571e37432249ea7fb655
raogpip <vitalik.fedorenko.2018@gmail.com>
2025-02-12 10:54:34 +0100

    Notifications page markup

--- resources/js/Pages/Notification/Index.vue
+++ resources/js/Pages/Notification/Index.vue
@@ -1,1 +1,38 @@
-<template>Hello!</template>
+<template>
+    <h1 class="text-3xl mb-4">Your Notifications</h1>
+    '
+
+    <section
+        v-if="notifications.data.length"
+        class="text-gray-700 dark:text-gray-400"
+    >
+        <div
+            v-for="notification in notifications.data"
+            :key="notification.id"
+            class="border-b border-gray-200 dark:border-gray-800 py-4 flex justify-between items-center"
+        >
+            <div>
+                <span
+                    v-if="notification.type === 'App\\Notifications\\OfferMade'"
+                    >Offer <Price :price="notification.data.amount" />
+                </span>
+            </div>
+            <div>
+                <button
+                    v-if="!notification.read_at"
+                    class="btn-outline text-xs font-medium uppercase"
+                >
+                    Mark as read
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import Price from "@/Components/Price.vue";
+
+const props = defineProps({
+    notifications: Object,
+});
+</script>
